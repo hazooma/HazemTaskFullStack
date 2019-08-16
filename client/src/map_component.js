08a1a127024b78d2ef82eb768f327038b4f8e7b4
Xsozm <hazem29778865@gmail.com>
2019-08-16 16:38:14 +0200

    fixPOlyLines

--- client/src/map_component.js
+++ client/src/map_component.js
@@ -10,11 +10,10 @@ const getRouteSummary = locations => {
   );
   return `${from} - ${to}`;
 };
-let Map;
 let polylines = [];
 const MapComponent = props => {
   // const map = useRef();
-  let [map, setMap] = useState();
+  let [currentMap, setcurrentMap] = useState();
 
   const [locations, setLocations] = useState();
   const [link, setLink] = useState("http://localhost:3000");
@@ -33,7 +32,7 @@ const MapComponent = props => {
 
   // Initialize map. always fixed
   useEffect(() => {
-    Map = new L.Map("mapid");
+    const map = new L.Map("mapid");
     const osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
     const attribution =
       'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
@@ -42,23 +41,23 @@ const MapComponent = props => {
       maxZoom: 12,
       attribution
     });
-    Map.setView(new L.LatLng(52.51, 13.4), 9);
-    Map.addLayer(osm);
-    setMap(Map);
+    map.setView(new L.LatLng(52.51, 13.4), 9);
+    map.addLayer(osm);
+    setcurrentMap(map);
   }, []);
 
   // Update location data on map.
   useEffect(() => {
-    if (!map || !locations) {
+    if (!currentMap || !locations) {
       return; // If map or locations not loaded yet.
     }
     if (polylines.length > 0) {
-      polylines.forEach(pl => {
-        map.remove(pl);
-      });
-      polylines = [];
+      polylines.forEach(pl => {});
     }
-
+    // console.log(currentMap);
+    polylines.forEach(pl => {
+      currentMap.removeLayer(pl);
+    });
     locations.forEach(segment => {
       // TODO(Task 1): Replace the single red polyline by the different segments on the map.
       const latlons = segment.locations.map(({ lat, lon }) => [lat, lon]);
@@ -66,14 +65,13 @@ const MapComponent = props => {
         color: colors[segment.segmentNumber] || "black"
       })
         .bindPopup(getRouteSummary(segment.locations))
-        .addTo(map);
-
+        .addTo(currentMap);
       polylines.push(polyline);
-      map.fitBounds(polyline.getBounds());
+      currentMap.fitBounds(polyline.getBounds());
     });
 
-    return () => map;
-  }, [locations, map, link]);
+    return () => currentMap;
+  }, [locations, currentMap, link]);
 
   return (
     <div>
